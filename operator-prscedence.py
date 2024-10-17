7b8ecbbee16c7c61c05da555f4c97e0e952ff5c3
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-10-17 20:48:42 +0530

    Lesson.8

---
+++ operator-prscedence.py
@@ -0,0 +1,16 @@
+v=4                                                                                                          
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y 
+
+print('value of (v+w) * x/y is',z)
+
+name = "Zain"
+age = 0
+
+if name == "Zain" or name =="shubha" and age >=2 :
+    print("Hello! welcome")
+else:
+    print("Good bye!")
